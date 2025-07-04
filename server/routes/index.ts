32228e4ab58953f77fb05fec02d1cea53a040a5f
Henry Iwanejko <henry.iwanejko@justice.gov.uk>
2025-07-04 15:23:45 +0100

    CPR-780 Add template table

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -3,12 +3,33 @@ import { Router } from 'express'
 import type { Services } from '../services'
 import { Page } from '../services/auditService'
 
+type Table = {
+  caption: string
+  head: Heading[]
+  rows: Row[]
+}
+
+type Heading = {
+  text: string
+}
+
+type Row = Item[]
+
+type Item = {
+  text: string
+}
+
 export default function routes({ auditService }: Services): Router {
   const router = Router()
 
   router.get('/', async (req, res, next) => {
+    const tableData: Table = {
+      caption: 'Needs Attention Clusters:',
+      head: [{ text: 'UUID' }, { text: 'Record Compostion' }],
+      rows: [[{ text: 'Test' }, { text: 'Test' }]],
+    }
     await auditService.logPageView(Page.EXAMPLE_PAGE, { who: res.locals.user.username, correlationId: req.id })
-    return res.render('pages/index')
+    return res.render('pages/index', tableData)
   })
 
   return router
