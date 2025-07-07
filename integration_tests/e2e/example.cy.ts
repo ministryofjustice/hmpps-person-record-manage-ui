a3d0d28cecd6e0fcd141481ee406702bf4c74506
samz-justice <sam.zaroug@justice.gov.uk>
2025-07-07 11:39:25 +0100

    CPR-780 update to role

--- integration_tests/e2e/example.cy.ts
+++ integration_tests/e2e/example.cy.ts
@@ -1,7 +1,7 @@
 context('Example feature', () => {
   beforeEach(() => {
     cy.task('reset')
-    cy.task('stubSignIn', { roles: ['PERSON_RECORD_MANAGE_ADMIN'] })
+    cy.task('stubSignIn', { roles: ['ROLE_PERSON_RECORD_MANAGE__ADMIN'] })
   })
 
   // it('Time from exampleApi is visible on page', () => {
