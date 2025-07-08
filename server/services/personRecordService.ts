c92d7c44b9ed5fc7dd9c1beb75b76c322c866ff9
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-07-08 10:23:16 +0100

    Revert "Update server/services/personRecordService.ts"
    This reverts commit 6384ab247257dc0cb36ab9285d844ce730ee56c5.

--- server/services/personRecordService.ts
+++ server/services/personRecordService.ts
@@ -4,6 +4,6 @@ export default class PersonRecordService {
   constructor(private readonly personRecordApiClient: PersonRecordApiClient) {}
 
   getCurrentTime() {
-    return this.personRecordApiClient.getCurrentTime()
+    return this.prisonRecordApiClient.getCurrentTime()
   }
 }
