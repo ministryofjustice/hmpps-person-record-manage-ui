09c7c0142358de29199505b9dbd26348c15ab601
samz-justice <sam.zaroug@justice.gov.uk>
2025-12-03 11:27:12 +0000

    CPR-985 added the rest of cluster view tests

--- integration_tests/pages/clusterPage.ts
+++ integration_tests/pages/clusterPage.ts
@@ -39,4 +39,14 @@ export default class ClusterPage extends AbstractPage {
     const hint = this.page.locator('.inset-information')
     await expect(hint).toHaveText(expected)
   }
+
+  async verifyEventLogTableHeader(index: number, expected: string) {
+    const logHeader = this.page.locator(`#event-log-table tr th:nth-of-type(${index})`)
+    await expect(logHeader).toHaveText(expected)
+  }
+
+  async verifyEventLogTableRow(rowIndex: number, columnIndex: number, expected: string) {
+    const row = this.page.locator(`#event-log-table tbody .govuk-table__row:nth-of-type(${rowIndex}) td:nth-of-type(${columnIndex})`)
+    await expect(row).toHaveText(expected)
+  }
 }
